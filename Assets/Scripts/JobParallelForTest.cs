01904b3629fdecc79505fcb508b9a9c1c8885342
Dmitry Timofeev <generalhi@gmail.com>
2025-02-09 22:23:02 +0300

    Job ParallelFor

---
+++ Assets/Scripts/JobParallelForTest.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public struct JobParallelFor : IJobParallelFor
+{
+    [ReadOnly]
+    public NativeArray<int> Input;
+
+    [WriteOnly]
+    public NativeArray<int> Output;
+
+    public void Execute(int i)
+    {
+        Output[i] = Input[0] + 1;
+    }
+}
+
+public class JobParallelForTest : MonoBehaviour
+{
+    void Start()
+    {
+        StartJob();
+    }
+
+    private void StartJob()
+    {
+        var length = 10;
+        var input = new NativeArray<int>(length, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+        var output = new NativeArray<int>(length, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+        for (var i = 0; i < length; i++)
+        {
+            input[i] = i;
+        }
+        var job = new JobParallelFor
+        {
+            Input = input,
+            Output = output
+        };
+        var handle = job.Schedule(length, 64);
+        handle.Complete();
+        Debug.Log($"[{input[0]}] [{output[0]}]");
+    }
+}

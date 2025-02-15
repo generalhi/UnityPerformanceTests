1ba6ea14e76d680eb3251fbb10b00a1561f191aa
Dmitry Timofeev <generalhi@gmail.com>
2025-02-15 23:25:22 +0300

    Refactoring

--- Assets/Scripts/TestsRunner/Tests/Fill/Fill_NativeArray_JobParallelFor_Unmanaged_Burst.cs
+++ Assets/Scripts/TestsRunner/Tests/Fill/Fill_NativeArray_JobParallelFor_Unmanaged_Burst.cs
@@ -10,7 +10,6 @@ namespace TestsRunner.Tests.Fill
     public class Fill_NativeArray_JobParallelFor_Unmanaged_Burst
     {
         [BurstCompile]
-        [NativeContainer]
         private unsafe struct JobParallelFor : IJobParallelFor
         {
             [NativeDisableUnsafePtrRestriction]
